af3941bfaa488d925e4e7e80b5be5fd089dd2403
chenzhang <zhangchen990622@gmail.com>
2024-05-04 14:54:42 +0200

    error

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,44 +1,61 @@
+
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+    <!-- show books list -->
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li v-for="(book, index) in books" :key="index" style="display:block">
+        {{index}}-{{book.name}}-{{book.author}}
+      </li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <!-- form to add a book -->
+    <form action="">
+      输入书名：<input type="text" placeholder="book name" v-model="inputBook.name"><br>
+      输入作者：<input type="text" placeholder="book author" v-model="inputBook.author"><br>
+    </form>
+    <button type="submit" @click="bookSubmit()">submit</button>
   </div>
 </template>
 
 <script>
+import {getBooks, postBook} from '../api/api.js'
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      // books list data
+      books: [
+        {name: 'test', author: 't'},
+        {name: 'test2', author: 't2'}
+      ],
+      // book data in the form
+      inputBook: {
+        "name": "",
+        "author": "",
+      }
+    }
+  },
+  methods: {
+    loadBooks () {
+      getBooks().then(response => {
+        console.log(response)
+        this.books = response.data
+      })
+    }, // load books list when visit the page
+    bookSubmit () {
+      postBook(this.inputBook).then(response => {
+        console.log(response)
+        this.loadBooks()
+      })
+    } // add a book to backend when click the button
+  },
+
+  created: function () {
+    this.loadBooks()
   }
 }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
+//import { createApp } from 'vue'
+//import App from './App.vue'
+// import Axios from 'axios';
+
+//const app = createApp(App);
+//const axiosInstance = Axios.create({
+   // withCredentials: true
+//})
+// 在 Vue 3 中，可以使用 provide 来提供属性
+//app.provide('axios', axiosInstance); // 在这里提供 axios 实例
+
+//app.mount('#app');
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.mount('#app');
